5e1799586ebda970895b1f365cb18d9e78fdea98
Kartik Raiyani <93818535+kartikraiyani03@users.noreply.github.com>
2023-08-27 22:57:22 +0530

    Add files via upload

---
+++ time.js
@@ -0,0 +1,66 @@
+let a = setTimeout(function () {
+    alert("after 1 sec")
+}, 1000)
+
+const sum = (a, b) => {
+    console.log("Running", a + b)
+    a + b + c
+}
+
+
+let time = sum(sum, 1000, 20, 10)
+
+console.log(a)
+document.write("work")
+
+
+let a = document.getElementById('box01')
+a.className = 'red'
+
+let cn = document.getElementById("box01")
+
+setTimeout(function () {
+    cn.className = 'red'
+
+}, 100)
+
+setTimeout(function () {
+    cn.className = 'yellow'
+
+}, 200)
+
+setTimeout(function () {
+    cn.className = 'green'
+
+}, 300)
+
+setTimeout(function () {
+    cn.className = 'blue'
+
+}, 400)
+
+setTimeout(function () {
+    cn.className = 'orange'
+
+}, 500)
+
+setTimeout(function () {
+    cn.className = 'purple'
+
+}, 600)
+
+
+while (condition);
+{
+    return a + b
+}
+
+// // const sum = (a,b)=>
+// // {
+// //     return a+b
+// // }
+
+// // Array.map(()=>
+// // {
+
+// // })
